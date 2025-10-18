8f1c7aa59153c9bc4f6e4d8315e8a54b7a6b4355
Sergey Romanov <serhromanov23@gmail.com>
2025-10-18 17:58:09 +0300

    Added advertisement.py

---
+++ app/schemas/advertisement.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
+
+class AdvertisementBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    price: float
+    author: str
+
+class AdvertisementCreate(AdvertisementBase):
+    pass
+
+class AdvertisementUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[str] = None
+    author: Optional[str] = None
+
+class Advertisement(AdvertisementBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
