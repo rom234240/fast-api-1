6bdc367609076eb0930d0d1d99028742dd8a26db
Sergey Romanov <serhromanov23@gmail.com>
2025-10-18 17:47:36 +0300

    Added advertisement.py

--- app/models/advertisement.py
+++ app/models/advertisement.py
@@ -3,6 +3,8 @@ from sqlalchemy.sql import func
 from app.database.database import Base
 
 class Advertisement(Base):
+    __tablename__ = "advertisements"
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(200), nullable=False)
     description = Column(Text)
