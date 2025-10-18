7d79dbca384e373902bc8129cad4c6602726ee24
Sergey Romanov <serhromanov23@gmail.com>
2025-10-18 17:47:13 +0300

    Added advertisement.py

---
+++ app/models/advertisement.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
+from sqlalchemy.sql import func
+from app.database.database import Base
+
+class Advertisement(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False)
+    description = Column(Text)
+    price = Column(Float, nullable=False)
+    author = Column(String(100), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
